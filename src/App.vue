596b3ebc231567b2069a54b33fcd317e14e574dc
Rahul Dey <rahuldey9623@gmail.com>
2023-05-01 12:41:03 +0530

    Add Bootstrap and Font Awesome Icons

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Phonebook</h1>
+  <i class="fa fa-car" style="font-size: 60px; color: red"></i>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+// Bootstrap Css and JavaScript
+import "bootstrap/dist/css/bootstrap.css";
+import bootstrap from "bootstrap/dist/js/bootstrap.bundle.js";
+
+createApp(App).use(bootstrap).mount("#app");
